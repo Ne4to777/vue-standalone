c25e81a22157dc1b03c4478cb91ae12b0b100064
Ne4to777 <ASAlekseev@dme.aero.corp>
2019-10-24 10:22:03 +0300

    chunks disable

--- vue.config.js
+++ vue.config.js
@@ -33,27 +33,8 @@ module.exports = {
 			'vue-router': 'VueRouter',
 			vuex: 'Vuex'
 		} : {},
-
-		// optimization: process.env.NODE_ENV === 'production' ? {
-		// 	runtimeChunk: 'single',
-		// 	splitChunks: {
-		// 		chunks: 'all',
-		// 		maxInitialRequests: Infinity,
-		// 		minSize: 0,
-		// 		cacheGroups: {
-		// 			vendor: {
-		// 				test: /[\\/]node_modules[\\/]/,
-		// 				name(module) {
-		// 					// get the name. E.g. node_modules/packageName/not/this/part.js
-		// 					// or node_modules/packageName
-		// 					const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1]
-
-		// 					// npm package names are URL-safe, but some servers don't like @ symbols
-		// 					return `npm.${packageName.replace('@', '')}`
-		// 				},
-		// 			},
-		// 		},
-		// 	},
-		// } : {},
+		optimization: {
+			splitChunks: false
+		}
 	}
 }
